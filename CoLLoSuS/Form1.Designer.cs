b3410762a2858a78102caef29fabc9e576dad214
ZeNyX <vlad.lutchyn1303@gmail.com>
2017-12-29 21:23:55 +0300

    CoLLoSuS
    LogIn Form

---
+++ CoLLoSuS/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace CoLLoSuS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.WLCME_label = new System.Windows.Forms.Label();
+            this.SK_label = new System.Windows.Forms.Label();
+            this.LogIn_form = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-62, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // WLCME_label
+            // 
+            this.WLCME_label.AutoSize = true;
+            this.WLCME_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.WLCME_label.ForeColor = System.Drawing.SystemColors.Control;
+            this.WLCME_label.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.WLCME_label.Location = new System.Drawing.Point(49, 110);
+            this.WLCME_label.Name = "WLCME_label";
+            this.WLCME_label.Size = new System.Drawing.Size(260, 24);
+            this.WLCME_label.TabIndex = 3;
+            this.WLCME_label.Text = "Welcome to the CoLLoSuS";
+            this.WLCME_label.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // SK_label
+            // 
+            this.SK_label.AutoSize = true;
+            this.SK_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SK_label.ForeColor = System.Drawing.SystemColors.Control;
+            this.SK_label.Location = new System.Drawing.Point(64, 197);
+            this.SK_label.Name = "SK_label";
+            this.SK_label.Size = new System.Drawing.Size(202, 16);
+            this.SK_label.TabIndex = 4;
+            this.SK_label.Text = "Please enter your serial key";
+            this.SK_label.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LogIn_form
+            // 
+            this.LogIn_form.Location = new System.Drawing.Point(35, 216);
+            this.LogIn_form.Name = "LogIn_form";
+            this.LogIn_form.Size = new System.Drawing.Size(274, 20);
+            this.LogIn_form.TabIndex = 5;
+            this.LogIn_form.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(192, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "LogIn";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "CoLLoSuS";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(364, 463);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LogIn_form);
+            this.Controls.Add(this.SK_label);
+            this.Controls.Add(this.WLCME_label);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CoLLoSuS";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label WLCME_label;
+        private System.Windows.Forms.Label SK_label;
+        private System.Windows.Forms.TextBox LogIn_form;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ CoLLoSuS/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoLLoSuS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
